9b6b5063a2913905c196037c1e3ffbb66bef6fa2
Gianni Lunardi <gianni.lunardi@unibz.it>
2026-07-03 10:29:51 +0200

    add multiple services for simulation, exp host and exp robot

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,7 @@ PLANNER_WS="${PLANNER_WS:-/planner_ws}"
 PLANNER_LAUNCH_PACKAGE="${PLANNER_LAUNCH_PACKAGE:-field_local_planner_ros}"
 PLANNER_LAUNCH_FILE="${PLANNER_LAUNCH_FILE:-rmp.launch}"
 CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
+CATKIN_EXTEND_PATH="${CATKIN_EXTEND_PATH:-/opt/ros/${ROS_DISTRO}}"
 
 source "/opt/ros/${ROS_DISTRO}/setup.bash"
 
@@ -15,7 +16,7 @@ shift || true
 case "${MODE}" in
   build)
     cd "${PLANNER_WS}"
-    catkin config --cmake-args "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+    catkin config --extend "${CATKIN_EXTEND_PATH}" --cmake-args "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
     exec catkin build "$@"
     ;;
   run)
